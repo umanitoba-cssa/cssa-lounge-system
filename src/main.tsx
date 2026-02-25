56d1954d3374393c5e7e6d62f45d12951a5a2215
travisfriesen <github@travisfriesen.ca>
2026-02-24 18:21:43 -0600

    feat: initial commit in plugin base repo

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+createRoot(document.getElementById('root')!).render(
+	<BrowserRouter>
+		<Routes>
+			<Route
+				path={`/`}
+				element={<App />}
+			/>
+		</Routes>
+	</BrowserRouter>
+)
